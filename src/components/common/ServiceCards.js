25d7d59156611ac99d4381ea0d6ae9220ed66a33
Parth Paresh Champaneria <pr514457@dal.ca>
2023-02-26 20:59:05 -0400

    fixed warnings

--- src/components/common/ServiceCards.js
+++ src/components/common/ServiceCards.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card, Button, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const ServiceCard = ({ name, image, description, link, feature, pagelink }) => {
