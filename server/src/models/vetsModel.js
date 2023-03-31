7c81533c827455b3bc0713612cfff88fed5a2713
Parth Champaneria <pr514457@dal.ca>
2023-03-31 12:48:36 -0300

    Update vetsModel.js

--- server/src/models/vetsModel.js
+++ server/src/models/vetsModel.js
@@ -29,18 +29,47 @@ const vetSchema = new mongoose.Schema({
 
 });
 
-module.exports = mongoose.model("Vets", vetSchema);
+/**
+ * @author Parth Champaneria
+ * @author Jaivik Tailor
+ */
 
-const vet = mongoose.model("vets", vetSchema);
+const mongoose = require("mongoose");
 
-//get the list of all the vets from the database
-exports.getVets = async () => {
-  const vetList = await vet.find({});
-  return vetList;
-};
+//defining schema for the vets collection
+const vetSchema = new mongoose.Schema({
+  // vet_name: { type: String, required: true },
+  //_id: { type: ObjectId, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  photo:{type : Buffer},
+  otp:{type : Number},
+  phone: { type: String, required: true },
+  // photo_url: { type: String },
+  license_number: { type: String, required: true },
+  qualification: { type: String, required: true },
+  experience: { type: Number, required: true },
+  clinic_license_number: { type: String, required: true },
+  clinic_address: { type: String, required: true },
+  clinic_photo_url: { type: String },
+  rating: { type: Number, required: true },
+  fees: { type: Number, required: true },
+  clinic_name: { type: String, required: true },
 
-//get the vet by id from the database.
-exports.getVetById = async (id) => {
-  const vetList = await vet.findById(id);
-  return vetList;
+});
+
+const vet = mongoose.model("Vets", vetSchema);
+
+module.exports = {
+  vet,
+  getVets: async () => {
+    const vetList = await vet.find({});
+    return vetList;
+  },
+  getVetById: async (id) => {
+    const vetList = await vet.findById(id);
+    return vetList;
+  }
 };
